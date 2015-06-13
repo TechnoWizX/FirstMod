6caef7b9242234d7d29fa4d356013dd43a9060f8
TechnoWizX <technowizx@gmail.com>
2015-06-13 18:05:57 +0100

    Using a reference file

--- src/main/java/com/technowizx/firstmod/FirstMod.java
+++ src/main/java/com/technowizx/firstmod/FirstMod.java
@@ -1,16 +1,17 @@
 package com.technowizx.firstmod;
 
 import com.technowizx.firstmod.proxy.IProxy;
+import com.technowizx.firstmod.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="FirstMod", name="First Mod", version="1.7.10-1.0")
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class FirstMod
 {
-    @Mod.Instance("FirstMod")
+    @Mod.Instance(Reference.MOD_ID)
     public static FirstMod instance;
 
     @SidedProxy(clientSide = "com.technowizx.firstmod.proxy.ClientProxy", serverSide = "com.technowizx.firstmod.proxy.ServerProxy")

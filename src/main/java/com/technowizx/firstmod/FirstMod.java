0ff71fe44fc07e30a636e0317776140a325da0e1
TechnoWizX <technowizx@gmail.com>
2015-06-13 17:51:50 +0100

    Update

--- src/main/java/com/technowizx/firstmod/FirstMod.java
+++ src/main/java/com/technowizx/firstmod/FirstMod.java
@@ -1,6 +1,8 @@
 package com.technowizx.firstmod;
 
+import com.technowizx.firstmod.proxy.IProxy;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -10,6 +12,9 @@ public class FirstMod
 {
     @Mod.Instance("FirstMod")
     public static FirstMod instance;
+
+    @SidedProxy(clientSide = "com.technowizx.firstmod.proxy.ClientProxy", serverSide = "com.technowizx.firstmod.proxy.ServerProxy")
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
